645e32d3e247135f666b2fbe9dabb292fb8d262c
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-05-05 14:45:43 +0200

    Add strict_variables support in unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
+
+  c.before :each do
+    if ENV['STRICT_VARIABLES'] == 'yes'
+      Puppet.settings[:strict_variables]=true
+    end
+  end
 end
 
 shared_examples :compile, :compile => true do
